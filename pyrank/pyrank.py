19f0226948079669caf600054a82623bbb03f26c
Grace Tang <tsmgrace@gmail.com>
2018-06-12 13:41:59 +0800

    add precision and average precision

--- pyrank/pyrank.py
+++ pyrank/pyrank.py
@@ -18,7 +18,7 @@ def ndcg(r):
 def dcg(r, n=None, exp=True):
     """
     input:
-        - r (array): list of relevance scores in rank order
+        - r (array): array of relevance scores in rank order
         - n (integer): DCG@n
         - exp (boolean): use exponential formula
     """
@@ -35,9 +35,43 @@ def dcg(r, n=None, exp=True):
     return np.sum(gain / discount)
 
 
+def precision(r, n=None):
+    """
+    input:
+        - r (array): array of boolean relevance scores in rank order
+        - n (integer): precision@n
+    """
+    if not all(i in [0,1] for i in r) or not all(isinstance(i,bool) for i in a):
+        raise TypeError('Wrong types, array should be boolean')
+    r = np.array(r)
+    if n:
+        r = r[:n]
+    return np.sum(r)
+
+def average_precision(r, n=None):
+    """
+    input:
+        - r (array): array of boolean relevance scores in rank order
+        - n (integer): precision@n
+    """
+    if not all(i in [0,1] for i in r) or not all(isinstance(i,bool) for i in r):
+        raise TypeError('Wrong types, array should be boolean')
+    r = np.array(r)
+    if n:
+        r = r[:n]
+    correct = 0
+    score = 0
+    for idx, i in enumerate(r):
+        if i:
+            correct += 1
+            print(i, correct, idx+1)
+            score += correct/(idx+1)
+    return score/correct
+
 
 
 if __name__=='__main__':
     r = [1.0, 1.2, 0.8, 0.4, 0.9]
-    print(dcg(r))
-    print(ndcg(r))
+    #print(dcg(r))
+    #print(ndcg(r))
+    print(average_precision([True, False, False, True]))
